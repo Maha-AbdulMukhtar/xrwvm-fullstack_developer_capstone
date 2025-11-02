95d7d6c75c0cb78f7b8ee6da32412abf9d80f7b4
Maha-AbdulMukhtar <mahashabdulmukhtar@gmail.com>
2025-11-02 13:29:51 -0500

    feat: Add user authentication system

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,18 +1,24 @@
 # Uncomment the imports before you add the code
-# from django.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-# from . import views
+from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
+    path('register', views.registration, name='register'),
 
     # path for login
-    # path(route='login', view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
+
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
+    # path('dealer/<int:dealer_id>/reviews/', views.get_dealer_reviews, name='dealer_reviews'),
 
     # path for add a review view
+    # path('add_review/<int:dealer_id>/', views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
